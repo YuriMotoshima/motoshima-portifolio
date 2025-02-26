551d656f3938f57e344f1090bfec1e1b5fd7d132
Yuri Motoshima <yuri.motoshima@accenture.comm>
2025-02-26 15:27:30 -0300

    add ejs data

--- data/data_html.js
+++ data/data_html.js
@@ -1,4 +1,9 @@
 module.exports = {
     homeText: "Olá, eu sou o Yuri Motoshima.\n" + 
-              "Sou desenvolvedor Python."
+              "Sou desenvolvedor Python.",
+    aboutText: "Sou desenvolvedor Python BackEnd, em geral trabalhando em Delivery sou acostumado a solucionar problemas em demandas, bem como criar soluções do zero.\n"+
+                "Atualmente atuo criando soluções com arquitetura de microserviços síncronos ou assíncronos, em nuvem, bem como desenvolvimento de APIs Rest em containers, integradas a Data Warehouse (BigQuery) ou bancos SQL ou NoSQL.\n"+
+                "No entanto também já desenvolvi bastante ETL com grande volume de dados por execução (cerca de 4 a 6 milhões linhas), utilizando multiprocess em alguns cenários para distribuir e escalar o consumo de recursos disponível.\n"+
+                "\n" +
+                "Outras ferramentas que já trabalhei são Selenium, PyAutoGui, mas também já utilizei Digibee, Power BI, Most, Pipefy."
 };
--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ routes.get("/", function(req, res) {
 routes.get('/home', function(req, res) {
     console.log("Rendering index");
     // Renderizar o arquivo EJS com dados
-    return res.render('index', { homeText: data.homeText });
+    return res.render('index', { homeText: data.homeText, aboutText: data.aboutText });
 });
 
 
